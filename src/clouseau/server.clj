568fd6cd054d98c5dfc8c7b6dac54b071732c6ae
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-03-11 10:18:12 +0100

    Fixed issue #19: User name should be stored in a cookie.

--- src/clouseau/server.clj
+++ src/clouseau/server.clj
@@ -1,41 +1,3 @@
-;;;
-;;;   Clouseau
-;;; 
-;;;    Copyright (C) 2015 Pavel Tisnovsky <ptisnovs@redhat.com>
-;;; 
-;;; Bytecode synth is free software; you can redistribute it and/or modify
-;;; it under the terms of the GNU General Public License as published by
-;;; the Free Software Foundation; either version 2, or (at your option)
-;;; any later version.
-;;; 
-;;; Bytecode synth is distributed in the hope that it will be useful, but
-;;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;;; General Public License for more details.
-;;; 
-;;; You should have received a copy of the GNU General Public License
-;;; along with Bytecode synth; see the file COPYING.  If not, write to the
-;;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-;;; 02110-1301 USA.
-;;; 
-;;; Linking this library statically or dynamically with other modules is
-;;; making a combined work based on this library.  Thus, the terms and
-;;; conditions of the GNU General Public License cover the whole
-;;; combination.
-;;; 
-;;; As a special exception, the copyright holders of this library give you
-;;; permission to link this library with independent modules to produce an
-;;; executable, regardless of the license terms of these independent
-;;; modules, and to copy and distribute the resulting executable under
-;;; terms of your choice, provided that you also meet, for each linked
-;;; independent module, the terms and conditions of the license of that
-;;; module.  An independent module is a module which is not derived from
-;;; or based on this library.  If you modify this library, you may extend
-;;; this exception to your version of the library, but you are not
-;;; obligated to do so. If you do not wish to do so, delete this
-;;; exception statement from your version.
-;;; 
-
 (ns clouseau.server)
 
 (require '[ring.util.response     :as http-response])
@@ -125,7 +87,7 @@
                 (form/submit-button {:class "btn btn-default"} "Remember me")]]))
 
 (defn render-navigation-bar-section
-    [package]
+    [package user-name]
     [:nav {:class "navbar navbar-inverse navbar-fixed-top" :role "navigation"}
         [:div {:class "container-fluid"}
             [:div {:class "row"}
@@ -143,7 +105,7 @@
                     ] ; ./navbar-header
                 ] ; col ends
                 [:div {:class "col-md-3"}
-                    (render-name-field "")
+                    (render-name-field user-name)
                     ]
             ] ; row ends
         ] ; /.container-fluid
@@ -167,12 +129,12 @@
         [:br]])
 
 (defn html-renderer
-    [products package package-descriptions ccs-description products-per-descriptions products-without-descriptions new-description]
+    [products package package-descriptions ccs-description products-per-descriptions products-without-descriptions new-description user-name]
     (page/xhtml
         (render-html-header package)
         [:body
             [:div {:class "container"}
-                (render-navigation-bar-section package)
+                (render-navigation-bar-section package user-name)
 
                 (if new-description
                     (show-info-about-update package))
@@ -210,12 +172,12 @@
     ))
 
 (defn html-renderer-descriptions
-    [descriptions]
+    [descriptions user-name]
     (page/xhtml
         (render-html-header nil)
         [:body
             [:div {:class "container"}
-                (render-navigation-bar-section nil)
+                (render-navigation-bar-section nil user-name)
                     (for [description descriptions]
                         [:div [:div {:class "label label-warning"} [:a {:href (str "../?package=" (get description :name) ) } (get description :name)]]
                               [:div {:class "alert alert-success"} (.replaceAll (str "" (get description :description)) "\\n" "<br />")]]
@@ -258,8 +220,12 @@
         (for [variant variants]
             [variant (for [product products :when (= variant (get package-descriptions product))] product)])))
 
+(defn get-user-name
+    [new-user-name old-user-name]
+    (or new-user-name old-user-name))
+
 (defn process
-    [package new-description]
+    [package new-description format new-user-name old-user-name]
     (if (and (not-empty-parameter? package) (not-empty-parameter? new-description))
         (store-ccs-description package new-description))
     (let [ccs-description               (read-ccs-description package)
@@ -267,27 +233,37 @@
           products-without-descriptions (get-products-without-descriptions products/products package-descriptions)
           products-with-descriptions    (get-products-with-descriptions products/products package-descriptions)
           products-per-description      (get-products-per-description package-descriptions products-with-descriptions)
-          html-output                   (html-renderer products/products package package-descriptions ccs-description products-per-description products-without-descriptions new-description)]
-        (-> (http-response/response html-output)
-            (http-response/content-type "text/html"))))
+          user-name                     (get-user-name new-user-name old-user-name)
+          html-output                   (html-renderer products/products package package-descriptions ccs-description products-per-description products-without-descriptions new-description user-name)]
+        (if user-name
+            (-> (http-response/response html-output)
+                (http-response/set-cookie :user-name user-name {:max-age 36000000})
+                (http-response/content-type "text/html"))
+            (-> (http-response/response html-output)
+                (http-response/content-type "text/html")))))
 
 (defn perform-normal-processing
     [request]
     (log-request-information request)
     (let [params              (request :params)
           cookies             (request :cookies)
+          format              (get params "format")
           package             (get params "package")
-          new-description     (get params "new-description")]
+          new-description     (get params "new-description")
+          new-user-name       (get params "user-name")
+          old-user-name       (get (get cookies "user-name") :value)]
           (println "Incoming cookies: " cookies)
-          (let [response (process package new-description)]
+          (let [response (process package new-description format new-user-name old-user-name)]
               (println "Outgoing cookies: " (get response :cookies))
-              response)))
+              response
+          )))
 
 (defn render-all-descriptions
     [request]
-    (let [descriptions (read-all-descriptions)]
+    (let [descriptions (read-all-descriptions)
+          user-name    (get (get (request :cookies) "user-name") :value)]
         (println descriptions)
-        (-> (http-response/response (html-renderer-descriptions descriptions))
+        (-> (http-response/response (html-renderer-descriptions descriptions user-name))
             (http-response/content-type "text/html"))))
 
 (defn return-file
