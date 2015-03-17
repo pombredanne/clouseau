5e2b4d2d5feac5e950ba3c6a85867028bcfa3cd8
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-03-17 11:56:05 +0100

    Fixed issue #39: Add script to create ccs_descriptions.

---
+++ tools/create_table_packages.sh
@@ -0,0 +1,3 @@
+cat create_table_packages.sql | sqlite3 ccs_descriptions.db
+mv ccs_descriptions.db ../
+
---
+++ tools/create_table_packages.sql
@@ -0,0 +1,11 @@
+create table packages (
+    name text not null,
+    description text not null
+);
+
+-- insert into "packages" values('libstoragemgmt','The libStorageMgmt library provides a vendor agnostic open so
+-- application programming interface (API) that enables management of storage
+-- arrays. The library includes a command line interface for interactive use and
+-- scripting (lsmcli). The library also has a daemon that is used for
+-- executing plug-ins in a separate process (lsmd).');
+
