77335e0f500b9812f259ab6a126056a1ee2be69b
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-04-16 14:45:05 +0200

    Changed copyright message in the module server.clj.

--- src/clouseau/server.clj
+++ src/clouseau/server.clj
@@ -3,18 +3,18 @@
 ;;; 
 ;;;    Copyright (C) 2015 Pavel Tisnovsky <ptisnovs@redhat.com>
 ;;; 
-;;; Bytecode synth is free software; you can redistribute it and/or modify
+;;; Clouseau is free software; you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
 ;;; the Free Software Foundation; either version 2, or (at your option)
 ;;; any later version.
 ;;; 
-;;; Bytecode synth is distributed in the hope that it will be useful, but
+;;; Clouseau is distributed in the hope that it will be useful, but
 ;;; WITHOUT ANY WARRANTY; without even the implied warranty of
 ;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ;;; General Public License for more details.
 ;;; 
 ;;; You should have received a copy of the GNU General Public License
-;;; along with Bytecode synth; see the file COPYING.  If not, write to the
+;;; along with Clouseau; see the file COPYING.  If not, write to the
 ;;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 ;;; 02110-1301 USA.
 ;;; 
