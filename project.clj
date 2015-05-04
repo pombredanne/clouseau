6137c44c9c6d7fc034854969c0f8a3f159fe4212
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-05-04 11:52:25 +0200

    Updated project file to use test2junit plugin

--- project.clj
+++ project.clj
@@ -37,20 +37,21 @@
 ;;; 
 
 (defproject clouseau "0.1.0-SNAPSHOT"
-  :description "Package finder"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [ring/ring-core "1.2.2"]
-                 [ring/ring-jetty-adapter "1.2.2"]
-                 [hiccup "1.0.4"]
-                 [org.clojure/tools.cli "0.3.1"]
-                 [org.clojure/java.jdbc "0.3.5"]
-                 [org.xerial/sqlite-jdbc "3.7.2"]]
-  :dev-dependencies [[lein-ring "0.8.10"]]
-  :plugins [[lein-ring "0.8.10"]
-            [codox "0.8.11"]]
-  :ring {:handler clouseau.core/app}
-  :main clouseau.core)
+    :description "Package finder"
+    :url "http://example.com/FIXME"
+    :license {:name "Eclipse Public License"
+              :url "http://www.eclipse.org/legal/epl-v10.html"}
+    :dependencies [[org.clojure/clojure "1.5.1"]
+                   [ring/ring-core "1.2.2"]
+                   [ring/ring-jetty-adapter "1.2.2"]
+                   [hiccup "1.0.4"]
+                   [org.clojure/tools.cli "0.3.1"]
+                   [org.clojure/java.jdbc "0.3.5"]
+                   [org.xerial/sqlite-jdbc "3.7.2"]]
+    :dev-dependencies [[lein-ring "0.8.10"]]
+    :plugins [[lein-ring "0.8.10"]
+              [codox "0.8.11"]
+              [test2junit "1.1.0"]]
+    :ring {:handler clouseau.core/app}
+    :main clouseau.core)
 
