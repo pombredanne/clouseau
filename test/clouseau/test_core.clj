aa0e31cf7c335e06c0cfe60bd03459edab730b0c
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-05-12 13:46:22 +0200

    Fixed issue #65: Add tests for the 'core' module

--- test/clouseau/test_core.clj
+++ test/clouseau/test_core.clj
@@ -20,3 +20,43 @@
     (testing "if the clouseau.core/-main definition exists."
         (is (callable? 'clouseau.core/-main))))
 
+(deftest test-start-server-existence
+    "Check that the clouseau.core/start-server definition exists."
+    (testing "if the clouseau.core/start-server definition exists."
+        (is (callable? 'clouseau.core/start-server))))
+
+(deftest test-get-port-existence
+    "Check that the clouseau.core/get-port definition exists."
+    (testing "if the clouseau.core/get-port definition exists."
+        (is (callable? 'clouseau.core/get-port))))
+
+(deftest test-cli-options-def-existence
+    "Check that the clouseau.core/cli-options definition exists."
+    (testing "if the hostname def exists"
+        ; check if variable exists and it's bounded
+        (is (bound? (find-var 'clouseau.core/cli-options)))))
+
+(deftest test-app-def-existence
+    "Check that the clouseau.core/app definition exists."
+    (testing "if the hostname def exists"
+        ; check if variable exists and it's bounded
+        (is (bound? (find-var 'clouseau.core/app)))))
+
+(deftest test-default-port-def-existence
+    "Check that the clouseau.core/default-port definition exists."
+    (testing "if the hostname def exists"
+        ; check if variable exists and it's bounded
+        (is (bound? (find-var 'clouseau.core/default-port)))))
+
+;
+; Tests for function behaviours
+;
+
+(deftest test-get-port
+    "Check the function clouseau.core/get-port."
+    (testing "the function clouseau.core/get-port."
+        (is (= (get-port nil)    "3000"))
+        (is (= (get-port "")    "3000"))
+        (is (= (get-port "1")    "1"))
+        (is (= (get-port "3000") "3000"))))
+
