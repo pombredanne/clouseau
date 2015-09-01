0d6e86ca81bcdd25e8aabcc6a0558ffbfcc6a97a
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-09-01 15:30:41 +0200

    Fixed namespace in test_html_renderer module

--- test/clouseau/test_html_renderer.clj
+++ test/clouseau/test_html_renderer.clj
@@ -36,7 +36,7 @@
 ;;; exception statement from your version.
 ;;; 
 
-(ns clouseau.test-core
+(ns clouseau.test-html-renderer
   (:require [clojure.test :refer :all]
             [clouseau.html-renderer :refer :all]))
 
